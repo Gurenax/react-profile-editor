61e965fa67bbd25554b8e9bb3258c5427801d978
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-29 11:53:00 +1100

    Add first name input text field

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+## Steps of React
+1. Render out hard coded data
+2. Model your data (similar to the process in Rails)
+3. Add state to your app
+4. Make render use app's state (remove hard-coding from render)
+5. Handle events from interactive elements (`onClick`, `onChange`)
+6. Connect those two methods that update the app's state (`this.setState`)
+
+
 ## Models
 
 ### User
--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const getFullName = (firstName, lastName) => {
+  return `${firstName} ${lastName}`
+}
+
 class App extends Component {
   state = {
     user: {
       firstName: 'Perry',
       lastName: 'Perkins',
-      profileImageURL: 'https://randomuser.me/api/portraits/men/10.jpg'
+      profileImageURL: 'https://randomuser.me/api/portraits/men/1.jpg'
     }
   }
 
+  onChangeFirstName = (event) => {
+    // Get the <input> (an HTMLInputElement)
+    const input = event.target
+    // Get the current inputted text from the field
+    const newFirstName = input.value
+    // Make changes to the state
+    this.setState( (prevState) => {
+      const user = prevState.user
+      // Create copy of user with new first name
+      user.firstName = newFirstName
+      return {
+        // this.state.user will be updated
+        user: user
+      }
+    })
+  }
+  
   render() {
+    const user = this.state.user
+
     return (
       <div className="App">
         <h1>React Profile Editor</h1>
-        <img src="https://randomuser.me/api/portraits/men/10.jpg" />
-        <p>Name: Perry Perkins</p>
+        <img src={user.profileImageURL} />
+        <p>Name: {getFullName(user.firstName, user.lastName)}</p>
+
+        <label>
+          First name:
+          {' '}
+          <input
+            value={ user.firstName }
+            onChange={ this.onChangeFirstName }
+            />
+        </label>
       </div>
     );
   }
